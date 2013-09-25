12d5c7ab9d3a950632042dba22586edee48c731a
gsingers <gsingers@apache.org>
2013-09-25 18:46:03 -0400

    give me some sugar

--- src/main/python/python.py
+++ src/main/python/python.py
@@ -1,5 +1,6 @@
 import optparse
 from flask import Flask
+from flask import url_for
 from flask import render_template
 from flask import request
 import pysolr
@@ -33,14 +34,18 @@ def process_solr_rsp(solr_rsp):
 def standard(name=None):
     # do a match all request
     query = "*:*"
+    start = 0
     if request.method == 'POST' and request.form['search_box']:
         query = request.form['search_box']
-    rows = 10
-    if request.args.get('row'):
-        rows = request.args.get('row')
+    else:
+        if request.args.get('q'):
+            query = request.args.get('q')
+
+    if request.args.get('start'):
+        start = request.args.get('start')
 
     app.logger.info("Query: " + query)
-    kwargs = {"qt": "/lucid", "facet": "true", "rows": rows, "fl":"*,score",
+    kwargs = {"qt": "/lucid", "facet": "true", "start": start, "fl":"*,score",
               "facet.range":["open", "close", "volume"],
               "facet.range.start":"0",
               "facet.range.end":"1000",
@@ -75,11 +80,21 @@ def standard(name=None):
     #current_page_number = (int) Math.ceil(start / (double) results_per_page) + (page_count > 0 ? 1 : 0);
     #
     #app.logger.info("Saw {0} result(s).".format(len(results)))
+    next_start = start+10
+    prev_start = max(start - 10, 0)
+    current_url = url_for('standard', start=str(start), q=query)
+    next_url = url_for('standard', start=str(next_start), q=query)
+    prev_url = url_for('standard', start=str(prev_start), q=query)
+    app.logger.info("Next: " + next_url)
     return render_template('standard.html', name=name, search_results=results,
                            raw_response=response,
+                           start=start,
+                           current_url=current_url,
                            the_facets=facets,
                            the_stats=stats,
                            the_query=query, current_page=current_page_number,
+                           next_url=next_url,
+                           prev_url=prev_url,
                            the_page_count=page_count)
 
 if __name__ == '__main__':
