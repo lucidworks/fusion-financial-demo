024dde67a0de043c3c5d95a429a37070a6b13b9d
JohnGUnderwood <john.underwood@lucidworks.com>
2018-10-07 13:57:41 +0100

    Getting responses for related news join processor on companies page.

--- app/src/main/java/twigkit/client/processor/response/FetchRelatedNews.java
+++ app/src/main/java/twigkit/client/processor/response/FetchRelatedNews.java
@@ -7,7 +7,6 @@ import twigkit.conf.ConfiguredPlatformProvider;
 import twigkit.model.*;
 import twigkit.platform.Platform;
 import twigkit.processor.ResponseProcessor;
-import twigkit.search.processors.response.DateFacetValueFormatter;
 
 import java.util.List;
 
@@ -21,34 +20,65 @@ public class FetchRelatedNews extends ResponseProcessor {
 
     private static final Logger logger = LoggerFactory.getLogger(FetchRelatedNews.class);
 
+    public static String PARAMETER_FILTER_SUFFIX = "filter-value-suffix";
+    public static String PARAMETER_FILTER_FIELD = "filter-field";
+    public static String PARAMETER_ID_FIELD = "id-field";
+    public static String PARAMETER_METADATA_FIELD = "metadata-field";
+
+    public static String PARAMETER_PIVOT = "pivot";
+
+    public static String PARAMETER_PLATFORM = "news-platform";
+
+    private static Boolean USE_PARTIAL = false;
+
     @Inject
     private ConfiguredPlatformProvider platformProvider;
 
     @Override
+    public void setup(){
+        if(!hasParameter(PARAMETER_FILTER_SUFFIX)){
+            setParameterValue(PARAMETER_FILTER_SUFFIX,"");
+        }
+
+//        Check if string that is passed in contains a wildcard
+        String filterSuffix = getParameterStringValue(PARAMETER_FILTER_SUFFIX);
+        if(filterSuffix.endsWith("*")){
+            USE_PARTIAL = true;
+            setParameterValue(PARAMETER_FILTER_SUFFIX,filterSuffix.substring(0, filterSuffix.length() - 1));
+        }
+    }
+
+    @Override
     public Response change(Response companies) {
 
-        String newsPlatformId = getParameterStringValue("news-platform");
+        String newsPlatformId = getParameterStringValue(PARAMETER_PLATFORM);
         Platform newsPlatform = platformProvider.get(newsPlatformId);
 
 
         Query newsQuery = new Query();
         newsQuery.setResultsPerPage(0);
         newsQuery.setFacets("");
-        newsQuery.addCustomParameter("facet.pivot", getParameterStringValue("pivot"));
+        newsQuery.addCustomParameter("facet.pivot", getParameterStringValue(PARAMETER_PIVOT));
         newsQuery.addCustomParameter("facet.pivot.mincount", "1");
 
         // Filter on the current companies
-        String dataSourceField = getParameterStringValue("datasource-field");
-        String fieldValueSuffix = getParameterStringValue("field-suffix");
-        String idField = getParameterStringValue("id-field");
+        String dataSourceField = getParameterStringValue(PARAMETER_FILTER_FIELD);
+        String fieldValueSuffix = getParameterStringValue(PARAMETER_FILTER_SUFFIX);
+
+        logger.debug("filter suffix: {}",fieldValueSuffix);
+
+        String idField = getParameterStringValue(PARAMETER_ID_FIELD);
         companies.getResults().forEach(r -> {
             Filter filter = new Filter(dataSourceField, r.getFields().get(idField).getValue().getActualAsString() + fieldValueSuffix);
+            if(USE_PARTIAL){
+                filter.setMatch(Filter.Match.RIGHT_PARTIAL);
+            }
             filter.setOptional(Filter.Optional.VALUE);
             newsQuery.addFilter(filter);
         });
 
         Response relatedNews = newsPlatform.search(newsQuery);
-        String facetName = getParameterStringValue("pivot").replaceAll(",", "-");
+        String facetName = getParameterStringValue(PARAMETER_PIVOT).replaceAll(",", "-");
         Facet facet = relatedNews.getFacet(facetName);
 
         logger.debug("Got back {} entries for facet {}", facet != null ? facet.getFilters().size() : 0, facetName);
@@ -56,12 +86,15 @@ public class FetchRelatedNews extends ResponseProcessor {
         //_lw_data_source_s-Company_Events_ss'].filters" ng-if="filter.val==result.fields.ticker_s.val[0]+'-press-release'">
 
         // Glue related news metadata onto each company record
-        String metadataFieldName = getParameterStringValue("metadata-field");
+        String metadataFieldName = getParameterStringValue(PARAMETER_METADATA_FIELD);
         companies.getResults().forEach(r -> {
             facet.getFilters().forEach(f -> {
                 String filterValue = f.getValue().getActualAsString();
                 String companyId = r.getFields().get(idField).getValue().getActualAsString();
-                if (filterValue.equalsIgnoreCase(companyId + fieldValueSuffix)) {
+//                Strip off stock exchange ID
+                String ticker = filterValue.split("\\.")[0];
+//                if (filterValue.equalsIgnoreCase(companyId + fieldValueSuffix))
+                if (ticker.equalsIgnoreCase(companyId)){
                     Field metadata = new Field(metadataFieldName);
                     logger.debug("Found {} related news for company {}", f.getRelatedFilters().size(), companyId);
                     List<FacetFilter> filters = f.getRelatedFilters();
